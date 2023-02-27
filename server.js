b938c91ab3f170a97e011c4cece5e41e74b1bfe7
BixbyIp <cip30@my.centennialcollege.ca>
2023-02-27 20:00:09 +0800

    27Feb2023

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var app = require('./app')
+var app = require('./server/config/app')
 var http = require('http')
 
 var port = normalisePort(process.env.PORT || '8080')
